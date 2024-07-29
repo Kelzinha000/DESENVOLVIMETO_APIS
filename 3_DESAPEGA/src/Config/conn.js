7d7c8c5899ab394394c8709e92e3db689242e9d2
Raquel <raquelbulhoes212@gmail.com>
2024-07-29 08:37:30 -0300

    tabela usuario criada

--- 3_DESAPEGA/src/Config/conn.js
+++ 3_DESAPEGA/src/Config/conn.js
@@ -1,22 +1,21 @@
-import mysql from "mysql2" //conexão com o banco de dados 
+    import mysql from "mysql2" //conexão com o banco de dados 
 
-const conn = mysql.createPool({
-    connectionLimit: 10, 
-    host:'localhost', 
-    user:'root', 
-    password:"Sen@iDev77!.",
-    database:"DESAPEGA", 
-    port:3306
+    const conn = mysql.createPool({
+        connectionLimit: 10, 
+        host:process.env.MYSQL_HOST, 
+        user:process.env.MYSQL_USER, 
+        password:process.env.MYSQL_PASSWORD,
+        database:process.env.MYSQL_DATABASE, 
+        port:process.env.MYSQL_PORT
 
+    })
 
-})
+    conn.query('SELECT 1 + 1 AS solution', (err, result, fields)=>{
+        if(err){
+            console.error(err)
+            return
+    }
+    console.log("The solution is: ",  result[0].solution)
+    })
 
-conn.query('SELECT 1 + 1 AS solution', (err, result, fields)=>{
-    if(err){
-        console.error(err)
-        return
-}
-console.log("The solution is: ",  result[0].solution)
-})
-
-export default conn;
+    export default conn;
--- 3_DESAPEGA/src/server.js
+++ 3_DESAPEGA/src/server.js
@@ -1,19 +1,25 @@
-import "dotenv/config";
-import express, { request, response } from "express";
+    import "dotenv/config";
+    import express, { request, response } from "express";
 
-// importar conexão
-import conn from "./Config/conn.js"
+    // importar conexão
+    import conn from "./Config/conn.js"
 
-const PORT = process.env.PORT;
+    const PORT = process.env.PORT;
 
-const app = express();
+    // importação dos módulos (tabela)
+    import "./Models/usuarioModel.js"
+    const app = express();
+    //404 
+    app.use((request, response)=>{
+        response.status(404).json({message: "Recurso não encntrado"})
+    })
+    // 404
+    // app.get("*", (request, response) => {
+    // response.send("Olá, Mundo!");
+    // });
 
-app.get("/", (request, response) => {
-  response.send("Olá, Mundo!");
-});
+    app.listen(PORT, () => {
+    console.log("Servidor on PORT" + PORT);
+    });
 
-app.listen(PORT, () => {
-  console.log("Servidor on PORT" + PORT);
-});
-
-// .env é um arquivo de texto porém não se usar virgula para passar o proximo
+    // .env é um arquivo de texto porém não se usar virgula para passar o proximo
